93cb3611dda18fa11e36b36d772f873d766e53b4
Jackson Santos <jackson.santos@gertec.com.br>
2025-09-30 09:57:47 -0300

    Closes #15: Implementação da classe abstrata como contrato e simulação de ropositories

--- src/domain/employee/use-cases/create-room.spec.ts
+++ src/domain/employee/use-cases/create-room.spec.ts
@@ -6,17 +6,35 @@ type Request = {
     price: number
     image: string
 }
+
+abstract class RoomRepository {
+    abstract create(room: Room): Room
+}
 class CreateRoomUseCase {
+
+    constructor(private roomRepository: RoomRepository) {}
+
     handle(data: Request) {
         const price = Money.create(data.price)
         const room = Room.create({...data, price})
+        this.roomRepository.create(room)
+        return room
+    }
+}
+
+class RoomMemory implements RoomRepository {
+    private rooms: Room[] = []
+
+    create(room: Room): Room {
+        this.rooms.push(room)
         return room
     }
 }
 
 describe('Create rooms', () => {
     test('Must create a room', () => {
-        const useCase = new CreateRoomUseCase()
+        const roomMemory = new RoomMemory()
+        const useCase = new CreateRoomUseCase(roomMemory)
         const room = useCase.handle({
             name: 'Suite Presidencial',
             price: 120000,
